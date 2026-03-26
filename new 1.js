24900e2de71dff67d3851a482e19bef3c906ad9a
sg0093-247 <sg0093@srmist.edu.in>
2026-03-26 08:19:35 +0530

    Add files via upload

---
+++ new 1.js
@@ -0,0 +1,18 @@
+/**
+ * This class demonstrates a simple Java application that prints a string literal to the console.
+ * It serves as an introduction to basic Java concepts like classes, main methods, and static members.
+ * * @author Snehal
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * The entry point of the application.
+     * * @param args Command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        // Use System.out.println to print the String literal "OOPS"
+        // OOPS stands for Object-Oriented Programming System
+        System.out.println("OOPS");
+    }
+}
